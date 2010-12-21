2e1846e5ff2f7ab0cd3f49109897d02239ba1d6a
Tom Parker <palfrey@tevp.net>
2010-12-21 00:05:32 +0000

    Add initial work with Robolectric testing

---
+++ test/net/tevp/tubejourney/TubeJourneyTest.java
@@ -0,0 +1,14 @@
+package net.tevp.tubejourney;
+
+import org.junit.runner.RunWith;
+import org.junit.Test;
+import com.xtremelabs.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+public class TubeJourneyTest
+{
+	@Test
+	public void foo()
+	{
+	}
+}
